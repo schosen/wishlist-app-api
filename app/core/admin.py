8d8a150a587b828a566cee56acf5ad28a1ccbf1c
Sarah <sarahchosen@hotmail.co.uk>
2023-10-03 20:15:07 +0100

    api documentation drf

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,7 +11,7 @@ from core import models
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
-    list_display = ['email', 'first_name', 'uuid']
+    list_display = ['email', 'first_name', 'last_name', 'uuid']
     fieldsets = (
         (None, {'fields': ('email', 'password',)}),
         (_('Personal Info'),
@@ -37,6 +37,24 @@ class UserAdmin(BaseUserAdmin):
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'first_name',
+                'last_name',
+                'password1',
+                'password2',
+                'birthday',
+                'gender',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
 
 
 
